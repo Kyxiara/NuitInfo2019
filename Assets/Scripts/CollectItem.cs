fa1a640574dc839890c1fffa4be3e5a8056639ac
ClemiNeko <c.lebras776@laposte.net>
2019-12-06 03:20:40 +0100

    Added collectItem.

---
+++ Assets/Scripts/CollectItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{
+    private Inventory _inventory;
+
+    public Item item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inventory = Inventory.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _inventory.Add(item);
+            Destroy(this.gameObject);
+        }
+    }
+}
