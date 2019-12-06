ce3541ae73233f7da9f20a9fcf8899461c24a7bf
Carbonnier Mathieu <m.carbonnier73@gmail.com>
2019-12-06 03:48:29 +0100

    Add 2 items with the CS files who give items from a png

---
+++ Assets/Scripts/GiveItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class GiveItem : MonoBehaviour
+{
+    public Item itemToGive;
+    private bool alreadyGive = false;
+    private Inventory _inventory;
+
+    private void Start()
+    {
+        _inventory = Inventory.instance;
+    }
+
+    public bool give()
+    {
+        if (alreadyGive) return false;
+        _inventory.Add(itemToGive);
+        alreadyGive = true;
+        return true;
+    }
+
+}
