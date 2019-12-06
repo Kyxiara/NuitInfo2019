47c1f79d08a927822abeb30c30e0667fac49ffc0
ClemiNeko <c.lebras776@laposte.net>
2019-12-06 01:32:59 +0100

    Dialog quelque chose.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,32 +12,37 @@ public class Player : MonoBehaviour
 
     public GameObject points;
     private GameObject pointsGO;
+    private DialogueManager dialogueManager;
 
     Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogueManager = FindObjectOfType<DialogueManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (!dialogueManager.dialogueActive)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        distance += Mathf.Abs(movement.x) + Mathf.Abs(movement.y);
-        if (distance > 1000f && pointsGO == null){
-            distance = 0;
-            var pos = new Vector3(0, 1.5f, 0);
-            pointsGO = Instantiate(points, pos + transform.position, Quaternion.identity);
-            pointsGO.transform.parent = transform;
-            StartCoroutine(DisplayPoints());
+            distance += Mathf.Abs(movement.x) + Mathf.Abs(movement.y);
+            if (distance > 1000f && pointsGO == null)
+            {
+                distance = 0;
+                var pos = new Vector3(0, 1.5f, 0);
+                pointsGO = Instantiate(points, pos + transform.position, Quaternion.identity);
+                pointsGO.transform.parent = transform;
+                StartCoroutine(DisplayPoints());
+            }
         }
     }
 
