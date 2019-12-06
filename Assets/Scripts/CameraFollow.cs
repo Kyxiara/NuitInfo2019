1dcd7899b8d76a37823f804ea76790a87f4ef599
Kelly Narboux <kellynarboux@hotmail.fr>
2019-12-06 01:24:45 +0100

    Add limits zone

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform target;
+
+    private float xMax, xMin, yMin, yMax;
+
+    public Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Vector3 minTile = tilemap.CellToWorld(tilemap.cellBounds.min);
+        Vector3 maxTile = tilemap.CellToWorld(tilemap.cellBounds.max);
+
+        SetLimits(minTile, maxTile);
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), -10);
+    }
+
+    private void SetLimits(Vector3 minTile, Vector3 maxTile)
+    {
+        Camera cam = Camera.main;
+
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        xMin = minTile.x + width / 2;
+        xMax = maxTile.x - width / 2;
+
+        yMin = minTile.y + height / 2;
+        yMax = maxTile.y - height / 2;
+    }
+
+}
